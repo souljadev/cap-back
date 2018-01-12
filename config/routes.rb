d6ecc7265e9107ad8479614b72f33acf6ba395a8
souljadev <brendan.moy@gmail.com>
2018-01-12 15:43:07 -0500

    Restrict routes for new and edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts, except: %i[new edit]
   resources :posts, except: %i[new edit]
   resources :examples, except: %i[new edit]
   post '/sign-up' => 'users#signup'
